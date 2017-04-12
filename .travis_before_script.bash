707b86b43ba2a0bda1d27ce2019f798d9d79005a
Dave Coleman <dave@dav.ee>
2017-04-12 16:05:42 -0400

    Update Baxter for Kinetic (#59)
    * Baxter: Change virtual_joint to base
    Baxter: Remove duplicate baxter.srdf
    Baxter: Sync with latest setup assistant templates
    
    * Fix Baxter setup assistant roslaunch to work with xacro files
    
    * Fix deprecation MoveGroupExecutionService
    
    * Disable mongodb warehouse_ros functionality because not available via binary
    
    * Updates for Baxter MoveIt! config
     - Disable always in collision link between head and display
     - Make world_joint passive to stop MoveIt! warning
     - Move octomap settings to always be present to suppress warnings
     - Update Rviz config
    
    * Change baxter urdf to kinetic-devel branch
    
    * Change branch of moveit_ci for debugging

--- .travis_before_script.bash
+++ .travis_before_script.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-wstool set baxter_common --git https://github.com/RethinkRobotics/baxter_common.git -v master -y
+wstool set baxter_common --git https://github.com/RethinkRobotics/baxter_common.git -v kinetic-devel -y
 wstool update baxter_common
 cat .rosinstall
